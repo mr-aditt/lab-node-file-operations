0a2e2c8678b5cc88282f8f7012b974a31ad1d64e
mr-aditt <mr.aditt@outlook.com>
2022-01-24 14:28:31 +0530

    Final commit

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
+const data = ({userid,username,name,year,qualification,password}) => {
+    return {
+        UserID : userid,
+        Username:username,
+        Name :name,
+        Year:year,
+        Qualification:qualification,
+        Password:password
+    }
+}
 
+
+module.exports ={data}
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const fs=require("fs");
+const os =require("os");
+const crypto =require("crypto");
+const {data}=require('./data')
 
+const password = userData=>crypto.createHmac('sha256', 'secret').update(userData).digest('hex');
+
+const userData = data({
+    password: password("prograd123"),
+    userid: 201,
+    name: "ProGrad",
+    username: "Developer",
+    year: 2020,
+    qualification: "PhD"
+})
+console.log(userData);
+fs.writeFileSync(`message.txt`, `ProGrad Details
+${Object.keys(userData).map(ele => `${ele} -> ${userData[ele]}`).join("\n")}`)
